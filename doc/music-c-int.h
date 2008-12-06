554d49ee99fb39a7df1e81f205565acd7c9c8634
Örjan Ekeberg <orjan@nada.kth.se>
2008-12-06 15:41:51 +0000

    Convert manual to CamelCase

--- doc/music-c-int.h
+++ doc/music-c-int.h
@@ -1,101 +1,108 @@
 /* Setup */
 
-MUSIC_setup *MUSIC_create_setup (int *argc, char ***argv);
+MUSIC_Setup *MUSIC_createSetup (int *argc, char ***argv);
 
 /* Communicators */
 
-MPI_Intracomm MUSIC_setup_communicator (MUSIC_setup *setup);
+MPI_Intracomm MUSIC_Setup_communicator (MUSIC_setup *setup);
 
 /* Port creation */
 
-MUSIC_cont_output_port *MUSIC_publish_cont_output (char *id);
-MUSIC_cont_input_port *MUSIC_publish_cont_input (char *id);
-MUSIC_event_output_port *MUSIC_publish_event_output (char *id);
-MUSIC_event_input_port *MUSIC_publish_event_input (char *id);
-MUSIC_message_output_port *MUSIC_publish_message_output (char *id);
-MUSIC_message_input_port *MUSIC_publish_message_input (char *id);
+MUSIC_ContOutputPort *MUSIC_publishContOutput (MUSIC_setup *setup,
+					       char *id);
+MUSIC_ContInputPort *MUSIC_publishContInput (MUSIC_setup *setup,
+					     char *id);
+MUSIC_EventOutputPort *MUSIC_publishEventOutput (MUSIC_setup *setup,
+						 char *id);
+MUSIC_EventInputPort *MUSIC_publishEventInput (MUSIC_setup *setup,
+					       char *id);
+MUSIC_MessageOutputPort *MUSIC_publishMessageOutput (MUSIC_setup *setup,
+						     char *id);
+MUSIC_MessageInputPort *MUSIC_publishMessageInput (MUSIC_setup *setup,
+						   char *id);
 
-void MUSIC_destroy_cont_output (MUSIC_cont_output_port* port);
-void MUSIC_destroy_cont_input (MUSIC_cont_input_port* port);
-void MUSIC_destroy_event_output (MUSIC_event_output_port* port);
-void MUSIC_destroy_event_input (MUSIC_event_input_port* port);
-void MUSIC_destroy_message_output (MUSIC_message_output_port* port);
-void MUSIC_destroy_message_input (MUSIC_message_input_port* port);
+void MUSIC_destroyContOutput (MUSIC_ContOutputPort* port);
+void MUSIC_destroyContInput (MUSIC_ContInputPort* port);
+void MUSIC_destroyEventOutput (MUSIC_EventOutputPort* port);
+void MUSIC_destroyEventInput (MUSIC_EventInputPort* port);
+void MUSIC_destroyMessageOutput (MUSIC_MessageOutputPort* port);
+void MUSIC_destroyMessageInput (MUSIC_MessageInputPort* port);
 
 /* General port methods */
 
-/* xxx = cont | event
-   ddd = output | input */
+/* Xxx = Cont | Event
+   Ddd = Output | Input */
 
-int MUSIC_xxx_ddd_port_is_connected (xxx_ddd_port *port);
-int MUSIC_message_ddd_port_is_connected (message_ddd_port *port);
-int MUSIC_xxx_ddd_port_has_width (xxx_ddd_port *port);
-int MUSIC_xxx_ddd_port_width (xxx_ddd_port *port);
+int MUSIC_XxxDddPort_isConnected (XxxDddPort *port);
+int MUSIC_MessageDddPort_isConnected (MessageDddPort *port);
+int MUSIC_XxxDddPort_hasWidth (XxxDddPort *port);
+int MUSIC_XxxDddPort_width (XxxDddPort *port);
 
 /* Mapping */
 
 /* No arguments are optional. */
 
-void MUSIC_cont_output_port_map (MUSIC_cont_output_port *port,
-				 MUSIC_cont_data *dmap,
-				 int max_buffered);
+void MUSIC_ContOutputPort_map (MUSIC_ContOutputPort *port,
+			       MUSIC_ContData *dMap,
+			       int maxBuffered);
 
-void MUSIC_cont_input_port_map (MUSIC_cont_input_port *port,
-				MUSIC_cont_data *dmap,
-				double delay,
-				int max_buffered,
-				int interpolate);
+void MUSIC_ContInputPort_map (MUSIC_ContInputPort *port,
+			      MUSIC_ContData *dMap,
+			      double delay,
+			      int maxBuffered,
+			      int interpolate);
 
-void MUSIC_event_output_port_map (MUSIC_event_output_port *port,
-				  MUSIC_index_map *indices,
-				  int max_buffered);
+void MUSIC_EventOutputPort_map (MUSIC_EventOutputPort *port,
+				MUSIC_IndexMap *indices,
+				int maxBuffered);
 
-typedef void MUSIC_event_handler (double t, int id);
+typedef void MUSIC_EventHandler (double t, int id);
 
-void MUSIC_event_input_port_map (MUSIC_event_input_port *port,
-				 MUSIC_index_map *indices,
-				 MUSIC_event_handler *handle_event,
-				 double acc_latency,
-				 int max_buffered);
+void MUSIC_EventInputPort_map (MUSIC_EventInputPort *port,
+			       MUSIC_IndexMap *indices,
+			       MUSIC_EventHandler *handleEvent,
+			       double accLatency,
+			       int maxBuffered);
 
-void MUSIC_message_output_port_map (MUSIC_message_output_port *port,
-				    int max_buffered);
+void MUSIC_MessageOutputPort_map (MUSIC_MessageOutputPort *port,
+				  int maxBuffered);
 
-typedef void MUSIC_message_handler (double t, void *msg, size_t size);
+typedef void MUSIC_MessageHandler (double t, void *msg, size_t size);
 
-void MUSIC_message_input_port_map (MUSIC_message_handler *handle_message,
-				   double acc_latency,
-				   int max_buffered);
+void MUSIC_MessageInputPort_map (MUSIC_MessageInputPort *port,
+				 MUSIC_MessageHandler *handleMessage,
+				 double accLatency,
+				 int maxBuffered);
 
 /* Index maps */
 
-MUSIC_permutation_index *MUSIC_create_permutation_index (int *indices,
-							 int size);
+MUSIC_PermutationIndex *MUSIC_createPermutationIndex (int *indices,
+						      int size);
 
-void MUSIC_destroy_permutation_index (MUSIC_permutation_index *index);
+void MUSIC_destroyPermutationIndex (MUSIC_PermutationIndex *index);
 
-MUSIC_linear_index *MUSIC_create_linear_index (int base_index,
-					       int size);
+MUSIC_LinearIndex *MUSIC_createLinearIndex (int baseIndex,
+					    int size);
 
-void MUSIC_destroy_linear_index (MUSIC_linear_index *index);
+void MUSIC_destroyLinearIndex (MUSIC_LinearIndex *index);
 
 /* Data maps */
 
 /* Exception: The map argument can take any type of index map. */
 
-MUSIC_array_data *MUSIC_create_array_data (void *buffer,
-					   MPI_Datatype type,
-					   void *map);
+MUSIC_ArrayData *MUSIC_createArrayData (void *buffer,
+					MPI_Datatype type,
+					void *map);
 
 /* Exception: MUSIC_create_linear_array_data corresponds to
    c++ music::array_data::array_data (..., ..., ..., ...) */
 
-MUSIC_array_data *MUSIC_create_linear_array_data (void *buffer,
-						  MPI_Datatype type,
-						  int base_index,
-						  int size);
+MUSIC_ArrayData *MUSIC_createLinearArrayData (void *buffer,
+					      MPI_Datatype type,
+					      int base_index,
+					      int size);
 
-void MUSIC_destroy_array_data (MUSIC_array_data *array_data);
+void MUSIC_destroyArrayData (MUSIC_ArrayData *arrayData);
 
 /* Configuration variables */
 
@@ -103,23 +110,23 @@ void MUSIC_destroy_array_data (MUSIC_array_data *array_data);
    Extra maxlen argument prevents buffer overflow.
    Result is terminated by \0 unless longer than maxlen - 1 */
 
-int MUSIC_config (MUSIC_setup *setup,
+int MUSIC_config (MUSIC_Setup *setup,
 		  char *name,
 		  char *result,
-		  size_t maxlen);
+		  size_t maxLen);
 
-int MUSIC_config (MUSIC_setup *setup, char *name, int *result);
+int MUSIC_config (MUSIC_Setup *setup, char *name, int *result);
 
-int MUSIC_config (MUSIC_setup *setup, char *name, double *result);
+int MUSIC_config (MUSIC_Setup *setup, char *name, double *result);
 
 /* Runtime */
 
-MUSIC_runtime *MUSIC_create_runtime (MUSIC_setup *setup, double h);
+MUSIC_Runtime *MUSIC_createRuntime (MUSIC_Setup *setup, double h);
 
-void MUSIC_tick (MUSIC_runtime *runtime);
+void MUSIC_tick (MUSIC_Runtime *runtime);
 
-double MUSIC_time (MUSIC_runtime *runtime);
+double MUSIC_time (MUSIC_Runtime *runtime);
 
 /* Finalization */
 
-void MUSIC_destroy_runtime (MUSIC_runtime *runtime);
+void MUSIC_destroyRuntime (MUSIC_Runtime *runtime);

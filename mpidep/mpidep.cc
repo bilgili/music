993f325ea7ed5d1ed600e09cb3f853c41cac5a2e
Mikael Djurfeldt <mikael@djurfeldt.com>
2009-02-11 19:38:09 +0000

    2009-02-11  Mikael Djurfeldt  <mikael@djurfeldt.com>
    	* mpidep: New directory for MPI dependent code.
    
    	* configure.ac: Generate mpidep/Makefile.
    
    mpidep:
    	* Makefile.am, mpidep.hh, mpidep.cc: New files.
    
    utils:
    	* music.cc: Moved mpi dependent code to ../mpidep/mpidep.cc.
    
    	* Makefile.am: Removed dependency on librudeconfig for all
    	utilities except the music launcher.
    
    	* Makefile.am (music_LDADD): Added dependency on libmpidep.a.

---
+++ mpidep/mpidep.cc
@@ -0,0 +1,126 @@
+/*
+ *  This file is part of MUSIC.
+ *  Copyright (C) 2007, 2008, 2009 INCF
+ *
+ *  MUSIC is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  MUSIC is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "config.h"
+
+#include <string>
+#include <sstream>
+#include <fstream>
+
+extern "C" {
+#include <stdlib.h>
+};
+
+#include "music/error.hh"
+
+#include "mpidep.hh"
+
+// Implementation-dependent code
+
+#ifdef HAVE_RTS_GET_PERSONALITY
+#define BGL
+#else
+#ifdef HAVE_OMPI_COMM_FREE
+#define OPENMPI
+#else
+#define MPICH
+#endif
+#endif
+
+#ifdef BGL
+#include <rts.h>
+#endif
+
+int
+getRank (int argc, char *argv[])
+{
+#ifdef BGL
+  BGLPersonality p;
+  rts_get_personality (&p, sizeof (p));
+  int pid = rts_get_processor_id ();
+  unsigned int rank, np;
+  rts_rankForCoordinates (p.xCoord, p.yCoord, p.zCoord, pid, &rank, &np);
+  return rank;
+#endif
+#ifdef MPICH
+  int rank;
+  const std::string rankopt = "-p4rmrank";
+  for (int i = argc - 2; i > 0; --i)
+    if (argv[i] == rankopt)
+      {
+	std::istringstream iss (argv[i + 1]);
+	iss >> rank;
+	return rank;
+      }
+  return 0;
+#endif
+#ifdef OPENMPI
+  char* vpid = getenv ("OMPI_MCA_ns_nds_vpid");
+  if (vpid == NULL)
+    vpid = getenv ("OMPI_COMM_WORLD_RANK");
+  if (vpid == NULL)
+    MUSIC::error ("getRank: unable to determine process rank");
+  std::istringstream iss (vpid);
+  int rank;
+  iss >> rank;
+  return rank;
+#endif
+}
+
+
+#ifdef MPICH
+std::string
+getSharedDir ()
+{
+  std::ostringstream dirname;
+  char* musicSharedDir = getenv ("MUSIC_SHARED_DIR");
+  if (musicSharedDir)
+    dirname << musicSharedDir;
+  else
+    {
+      char* home = getenv ("HOME");
+      dirname << home;
+    }
+  return dirname.str ();
+}
+#endif
+
+
+std::istream*
+getConfig (int rank, int argc, char** argv)
+{
+#ifdef MPICH
+  std::ostringstream fname;
+  fname << getSharedDir () << "/.musicconf";
+  std::string confname;
+  if (rank == 0)
+    {
+      std::ofstream f (fname.str ().c_str ());
+      confname = argv[1];
+      f << confname;
+    }
+  else
+    {
+      std::ifstream f (fname.str ().c_str ());
+      f >> confname;
+    }
+  return new std::ifstream (confname.c_str ());
+#else
+  return new std::ifstream (argv[1]);
+#endif
+}
--- utils/music.cc
+++ utils/music.cc
@@ -1,6 +1,6 @@
 /*
  *  This file is part of MUSIC.
- *  Copyright (C) 2007, 2008, 2009 CSC, KTH
+ *  Copyright (C) 2007, 2008, 2009 INCF
  *
  *  MUSIC is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -17,14 +17,11 @@
  */
 
 #include <string>
-#include <sstream>
-#include <fstream>
-#include <iostream>
 
-#include "../config.h"
+#include "config.h"
 
-#include "music/error.hh"
 #include "application_mapper.hh"
+#include "mpidep/mpidep.hh"
 
 extern "C" {
 #include <sys/types.h>
@@ -34,105 +31,6 @@ extern "C" {
 };
 
 using std::string;
-using std::ifstream;
-
-// Implementation-dependent code
-
-#ifdef HAVE_RTS_GET_PERSONALITY
-#define BGL
-#else
-#ifdef HAVE_OMPI_COMM_FREE
-#define OPENMPI
-#else
-#define MPICH
-#endif
-#endif
-
-#ifdef BGL
-#include <rts.h>
-#endif
-
-int
-getRank (int argc, char *argv[])
-{
-#ifdef BGL
-  BGLPersonality p;
-  rts_get_personality (&p, sizeof (p));
-  int pid = rts_get_processor_id ();
-  unsigned int rank, np;
-  rts_rankForCoordinates (p.xCoord, p.yCoord, p.zCoord, pid, &rank, &np);
-  return rank;
-#endif
-#ifdef MPICH
-  int rank;
-  const std::string rankopt = "-p4rmrank";
-  for (int i = argc - 2; i > 0; --i)
-    if (argv[i] == rankopt)
-      {
-	std::istringstream iss (argv[i + 1]);
-	iss >> rank;
-	return rank;
-      }
-  return 0;
-#endif
-#ifdef OPENMPI
-  char* vpid = getenv ("OMPI_MCA_ns_nds_vpid");
-  if (vpid == NULL)
-    vpid = getenv ("OMPI_COMM_WORLD_RANK");
-  if (vpid == NULL)
-    MUSIC::error ("getRank: unable to determine process rank");
-  std::istringstream iss (vpid);
-  int rank;
-  iss >> rank;
-  return rank;
-#endif
-}
-
-
-#ifdef MPICH
-std::string
-getSharedDir ()
-{
-  std::ostringstream dirname;
-  char* musicSharedDir = getenv ("MUSIC_SHARED_DIR");
-  if (musicSharedDir)
-    dirname << musicSharedDir;
-  else
-    {
-      char* home = getenv ("HOME");
-      dirname << home;
-    }
-  return dirname.str ();
-}
-#endif
-
-
-std::istream*
-getConfig (int rank, int argc, char** argv)
-{
-#ifdef MPICH
-  std::ostringstream fname;
-  fname << getSharedDir () << "/.musicconf";
-  std::string confname;
-  if (rank == 0)
-    {
-      std::ofstream f (fname.str ().c_str ());
-      confname = argv[1];
-      f << confname;
-    }
-  else
-    {
-      std::ifstream f (fname.str ().c_str ());
-      f >> confname;
-    }
-  return new std::ifstream (confname.c_str ());
-#else
-  return new ifstream (argv[1]);
-#endif
-}
-
-
-// Generic code
 
 void
 usage (int rank)
@@ -168,6 +66,8 @@ launch (MUSIC::Configuration* config, char** argv)
 int
 main (int argc, char *argv[])
 {
+  // predict the rank MPI::Init will give us using
+  // mpi implementation dependent code from ../mpidep
   int rank = getRank (argc, argv);
 
   opterr = 0; // handle errors ourselves
@@ -200,6 +100,8 @@ main (int argc, char *argv[])
 	}
     }
 
+  // extract the configuration file name using
+  // mpi implementation dependent code from ../mpidep
   std::istream* configFile = getConfig (rank, argc, argv);
 
   if (!configFile)

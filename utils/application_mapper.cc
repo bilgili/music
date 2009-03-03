7fa711a3ba0cce870108d13b55d36ed25cc8ca17
Örjan Ekeberg <orjan@nada.kth.se>
2009-03-03 17:30:44 +0000

    Restructuring repository

---
+++ utils/application_mapper.cc
@@ -0,0 +1,211 @@
+/*
+ *  This file is part of MUSIC.
+ *  Copyright (C) 2007, 2008, 2009 INCF
+ *
+ *  MUSIC is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  MUSIC is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+//#define MUSIC_DEBUG 1
+
+#include "music/error.hh"
+#include "music/debug.hh"
+
+#include "application_mapper.hh"
+
+namespace MUSIC {
+
+  // This is where we parse the configuration file
+
+  // NOTE: Could check here that obligatory parameters exists
+  ApplicationMapper::ApplicationMapper (std::istream* configFile, int rank)
+  {
+    rude::Config* cfile = new rude::Config ();
+    cfile->load (*configFile);
+
+    mapSections (cfile);
+    mapApplications ();
+    selectApplication (rank);
+    mapConnectivity (cfile);
+  }
+
+
+  void
+  ApplicationMapper::mapSections (rude::Config* cfile)
+  {
+    MUSIC::Configuration* defConfig = 0;
+    int nSections = cfile->getNumSections ();
+    for (int s = 0; s < nSections; ++s)
+      {
+	const char* name = cfile->getSectionNameAt (s);
+	cfile->setSection (name);
+	MUSIC::Configuration* config
+	  = new MUSIC::Configuration (name, s - 1, defConfig);
+
+	int nMembers = cfile->getNumDataMembers ();
+	for (int m = 0; m < nMembers; ++m)
+	  {
+	    const char* name = cfile->getDataNameAt (m);
+	    config->insert (name, cfile->getStringValue (name));
+	  }
+
+	if (s == 0)
+	  defConfig = config;
+	else
+	  configs.insert (std::make_pair (name, config));
+      }
+  }
+
+
+  void
+  ApplicationMapper::mapApplications ()
+  {
+    applications_ = new ApplicationMap ();
+    int leader = 0;
+    std::map<std::string, MUSIC::Configuration*>::iterator config;
+    for (config = configs.begin (); config != configs.end (); ++config)
+      {
+	int np;
+	config->second->lookup ("np", &np);
+	applications_->add (config->first, leader, np);
+	leader += np;
+      }
+  }
+
+
+  void
+  ApplicationMapper::selectApplication (int rank)
+  {
+    int rankEnd = 0;
+    ApplicationMap::iterator ai;
+    for (ai = applications_->begin (); ai != applications_->end (); ++ai)
+      {
+	rankEnd += ai->nProc ();
+	if (rank < rankEnd)
+	  {
+	    selectedName = ai->name ();
+	    MUSIC_LOG ("rank " << rank << " mapped as " << selectedName);
+	    return;
+	  }
+      }
+
+    // postpone error reporting to setup
+    // NOTE: consider doing error checking here
+
+    // for now, choose the first application
+    selectedName = applications_->begin ()->name ();
+  }
+
+
+  void
+  ApplicationMapper::mapConnectivity (rude::Config* cfile)
+  {
+    std::map<std::string, int> receiverPortCodes;
+    int nextPortCode = 0;
+    connectivityMap_ = new Connectivity ();
+    int nSections = cfile->getNumSections ();
+    for (int s = 0; s < nSections; ++s)
+      {
+	std::string secName (cfile->getSectionNameAt (s));
+	cfile->setSection (secName.c_str ());
+
+	int nConnections = cfile->getNumSourceDestMembers ();
+	for (int c = 0; c < nConnections; ++c)
+	  {
+	    std::string senderApp (cfile->getSrcAppAt (c));
+	    std::string senderPort (cfile->getSrcObjAt (c));
+	    std::string receiverApp (cfile->getDestAppAt (c));
+	    std::string receiverPort (cfile->getDestObjAt (c));
+	    std::string width (cfile->getWidthAt (c));
+
+	    if (senderApp == "")
+	      if (secName == "")
+		error ("sender application not specified for output port " + senderPort);
+	      else
+		senderApp = secName;
+	    if (receiverApp == "")
+	      if (secName == "")
+		error ("receiver application not specified for input port " + receiverPort);
+	      else
+		receiverApp = secName;
+	    if (senderApp == receiverApp)
+	      error ("port " + senderPort + " of application " + senderApp + " connected to the same application");
+
+	    // Generate a unique "port code" for each receiver port
+	    // name.  This will later be used during temporal
+	    // negotiation since it easier to communicate integers,
+	    // which have constant size, than strings.
+	    //
+	    // NOTE: This code must be executed in the same order in
+	    // all MPI processes.
+	    std::map<std::string, int>::iterator pos
+	      = receiverPortCodes.find (receiverPort);
+	    int portCode;
+	    if (pos == receiverPortCodes.end ())
+	      {
+		portCode = nextPortCode++;
+		receiverPortCodes.insert (std::make_pair (receiverPort,
+							  portCode));
+	      }
+	    else
+	      portCode = pos->second;
+
+	    ConnectivityInfo::PortDirection dir;
+	    ApplicationInfo* remoteInfo;
+	    if (selectedName == senderApp)
+	      {
+		dir = ConnectivityInfo::OUTPUT;
+		remoteInfo = applications_->lookup (receiverApp);
+	      }
+	    else if (selectedName == receiverApp)
+	      {
+		dir = ConnectivityInfo::INPUT;
+		remoteInfo = applications_->lookup (senderApp);
+	      }
+	    else
+	      continue;
+	    int w;
+	    if (width == "")
+	      w = ConnectivityInfo::NO_WIDTH;
+	    else
+	      {
+		std::istringstream ws (width);
+		if (!(ws >> w))
+		  error ("could not interpret width");
+	      }
+
+	    connectivityMap_->add (dir == ConnectivityInfo::OUTPUT
+				   ? senderPort
+				   : receiverPort,
+				   dir,
+				   w,
+				   receiverApp,
+				   receiverPort,
+				   portCode,
+				   remoteInfo->leader (),
+				   remoteInfo->nProc ());
+	  }
+      }
+  }
+
+
+  MUSIC::Configuration*
+  ApplicationMapper::config ()
+  {
+    Configuration* config = configs[selectedName];
+    config->setApplications (applications_);
+    config->setConnectivityMap (connectivityMap_);
+    return config;
+  }
+
+}
---
+++ utils/music.cc
@@ -0,0 +1,126 @@
+/*
+ *  This file is part of MUSIC.
+ *  Copyright (C) 2007, 2008, 2009 INCF
+ *
+ *  MUSIC is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  MUSIC is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <string>
+
+#include "config.h"
+
+#include "application_mapper.hh"
+#include "mpidep/mpidep.hh"
+
+extern "C" {
+#include <sys/types.h>
+#include <unistd.h>
+#include <getopt.h>
+#include <stdlib.h>
+#include <stdio.h>
+};
+
+using std::string;
+
+void
+usage (int rank)
+{
+  if (rank == 0)
+    {
+      std::cerr << "Usage: music [OPTION...] CONFIG" << std::endl
+		<< "`music' launches an application as part of a multi-simulator job." << std::endl << std::endl
+		<< "  -h, --help            print this help message" << std::endl << std::endl
+		<< "Report bugs to <mikael@djurfeldt.com>." << std::endl;
+    }
+  exit (1);
+}
+
+
+void
+launch (MUSIC::Configuration* config, char** argv)
+{
+  string binary;
+  config->lookup ("binary", &binary);
+  config->writeEnv ();
+  string wd;
+  if (config->lookup ("wd", &wd))
+    {
+      if (chdir (wd.c_str ()))
+	goto error_exit;
+    }
+  execvp (binary.c_str (), argv);
+
+ error_exit:
+  // if we get here, something is wrong
+  std::cerr << "Error during launching of binary " << binary << std::endl;
+  perror ("MUSIC");
+  exit (1);
+}
+
+
+int
+main (int argc, char *argv[])
+{
+  // predict the rank MPI::Init will give us using
+  // mpi implementation dependent code from ../mpidep
+  int rank = getRank (argc, argv);
+
+  opterr = 0; // handle errors ourselves
+  while (1)
+    {
+      static struct option longOptions[] =
+	{
+	  {"help",    no_argument,       0, 'h'},
+	  {0, 0, 0, 0}
+	};
+      /* `getopt_long' stores the option index here. */
+      int option_index = 0;
+
+      // the + below tells getopt_long not to reorder argv
+      int c = getopt_long (argc, argv, "+h", longOptions, &option_index);
+
+      /* detect the end of the options */
+      if (c == -1)
+	break;
+
+      switch (c)
+	{
+	case '?':
+	  break; // ignore unknown options
+	case 'h':
+	  usage (rank);
+
+	default:
+	  abort ();
+	}
+    }
+
+  // extract the configuration file name using
+  // mpi implementation dependent code from ../mpidep
+  std::istream* configFile = getConfig (rank, argc, argv);
+
+  if (!*configFile)
+    {
+      if (rank == 0)
+	std::cerr << "MUSIC: Couldn't open config file "
+		  << argv[1] << std::endl;
+      exit (1);
+    }
+
+  MUSIC::ApplicationMapper map (configFile, rank);
+  
+  launch (map.config (), argv);
+
+  return 0;
+}

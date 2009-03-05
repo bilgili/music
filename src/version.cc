40d729cb2fdc9faf91a05aa0313f656c64d59b76
Mikael Djurfeldt <mikael@djurfeldt.com>
2009-03-05 23:59:40 +0000

    2009-03-06  Mikael Djurfeldt  <mikael@djurfeldt.com>
    	* music/version.hh, version.cc: New files.
    
    	* Makefile.am: Added ditto.
    
    	* Makefile.am: Set shared library version 1.0.0. (Note that this
    	version number follows different rules than the MUSIC version.)

---
+++ src/version.cc
@@ -0,0 +1,30 @@
+/*
+ *  This file is part of MUSIC.
+ *  Copyright (C) 2009 INCF
+ *
+ *  MUSIC is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  MUSIC is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "music/version.hh"
+
+namespace MUSIC {
+
+  const char*
+  version ()
+  {
+    // NOTE: take from version.hh which should get it from configure
+    return "1.0.1";
+  }
+
+}

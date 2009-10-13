1a760894626625d1730568cc9418e3cd090faf3e
Mikael Djurfeldt <mikael@djurfeldt.com>
2009-10-13 19:41:49 +0000

    Updated

--- pymusic/helloworld.py
+++ pymusic/helloworld.py
@@ -7,8 +7,14 @@ import sys, music
 setup = music.Setup (sys.argv)
 from mpi4py import MPI
 
-hwmess = "Hello, World! I am process %d of %d with arg %s.\n"
+hwmess = "Hello, World! I am process %d of %d with argument %s.\n"
 comm = setup.communicator ()
 myrank = comm.Get_rank()
 nprocs = comm.Get_size()
 sys.stdout.write(hwmess % (myrank, nprocs, sys.argv[1]))
+
+# Entering runtime phase
+runtime = music.Runtime (setup, 1e-4)
+
+# Must end with a call to finalize ()
+runtime.finalize ()

43496177b257b251a95a96c693f5c726ab3fc6cc
mdjurfeldt <mdjurfeldt@b86cec79-47c1-4bcb-83d2-4e3c60c58a18>
2012-03-15 09:57:23 +0000

    2012-03-15  Mikael Djurfeldt  <mikael@djurfeldt.com>
    	* music: New directory. Python interface for configuring MUSIC.
    
    2012-03-15  Mikael Djurfeldt  <mikael@djurfeldt.com>
    
    	* music.py: New file.
    
    
    git-svn-id: http://svn.incf.org/svn/music/trunk@701 b86cec79-47c1-4bcb-83d2-4e3c60c58a18

---
+++ music/music.py
@@ -0,0 +1,73 @@
+#
+#  This file is part of MUSIC.
+#  Copyright (C) 2012 Mikael Djurfeldt
+#
+#  MUSIC is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  MUSIC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+# This API allows for querying the MPI rank and specification of the
+# information in MUSIC configuration files
+
+def predictRank ():
+    """
+    Returns the predicted MPI rank of this process
+    
+    :rtype: Integer
+    """
+    return 0
+
+
+def postponeSetup ():
+    """
+    Postpones processing of configuration info until the creation of
+    the first port.  Must be called before creation of MUSIC::Setup.
+    """
+    pass
+
+
+def define (varName, value):
+    """
+    Define configuration variable varName to value value.
+    """
+    pass
+
+
+def application (name):
+    """
+    Return configuration object for application name.  Create it if it
+    does not already exist.
+
+    :rtype: Application
+    """
+    return lookupCreate (name)
+
+
+def connect (fromPort, toPort):
+    """
+    Connect fromPort to toPort.
+    """
+    pass
+
+class Application (object):
+    def define (varName, value):
+        """
+        Define configuration variable varName to value value.
+        """
+        pass
+
+    def connect (fromPort, toPort):
+        """
+        Connect fromPort to toPort.
+        """
+        pass

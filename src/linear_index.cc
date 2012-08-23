10cdf832ef93487dc3af3e1158381c72c3ea0123
Mikael Djurfeldt <mikael@djurfeldt.com>
2012-08-23 08:18:45 +0000

    Whitespace

--- src/linear_index.cc
+++ src/linear_index.cc
@@ -15,7 +15,9 @@
  *  You should have received a copy of the GNU General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 #include "music/linear_index.hh"
+
 namespace MUSIC {
   
   LinearIndex::iterator::iterator (LinearIndex* li)
